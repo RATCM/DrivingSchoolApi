b650ab865bbaf2696504db69e766aa23eb315099
RATCM <isakbjoershol@gmail.com>
2026-03-05 22:36:14 +0100

    Add calender for students

---
+++ DrivingSchoolApi.Infrastructure/Database/Migrations/20260305213051_StudentCalender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrivingSchoolApi.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentCalender : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentTimeSlots",
+                columns: table => new
+                {
+                    StartDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentTimeSlots", x => new { x.StudentId, x.StartDateTime, x.EndDateTime });
+                    table.ForeignKey(
+                        name: "FK_StudentTimeSlots_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentTimeSlots");
+        }
+    }
+}
