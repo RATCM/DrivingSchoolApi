ed50cb1e2ebf6240d9e2d4ff376c78df1bb15946
RATCM <isakbjoershol@gmail.com>
2026-03-05 22:53:48 +0100

    Add admin user

---
+++ DrivingSchoolApi.Infrastructure/Configurations/AdminConfiguration.cs
@@ -0,0 +1,27 @@
+using DrivingSchoolApi.Domain.Entities;
+using DrivingSchoolApi.Domain.Keys;
+using DrivingSchoolApi.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DrivingSchoolApi.Infrastructure.Configurations;
+
+public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+{
+    public void Configure(EntityTypeBuilder<Admin> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .HasConversion(
+                key => key.Value,
+                value => AdminKey.Create(value));
+        
+        builder.Property(x => x.EmailAddress).HasConversion(
+            email => email.Address,
+            address => Email.Create(address));
+        
+        builder.Property(x => x.HashedPassword).HasConversion(
+            x => x.Hash,
+            x => PasswordHash.Create(x));
+    }
+}
