cccd73cb1add8863b32d07280e832486f03f3a34
ih8mondays <bjoerkobel@hotmail.dk>
2026-04-22 10:57:51 +0200

    test extension methods

---
+++ DrivingSchoolApi.Application.UnitTest/Extensions/TheoryLessonExtensions.cs
@@ -0,0 +1,38 @@
+﻿using DrivingSchoolApi.Domain.Entities;
+using DrivingSchoolApi.Domain.Keys;
+using DrivingSchoolApi.Domain.ValueObjects;
+using NUnit.Framework.Constraints;
+
+namespace DrivingSchoolApi.Application.UnitTest.Extensions;
+
+public static class TheoryLessonExtensions
+{
+    extension(TheoryLesson theoryLesson)
+    {
+        public static TheoryLesson CreateTestTheoryLesson(
+            DateTime dateTime,
+            Guid? id = null, 
+            Guid? schoolId = null,
+            Guid? instructorId = null
+            )
+        {
+            id ??= Guid.NewGuid();
+            schoolId ??= Guid.NewGuid();
+            instructorId ??= Guid.NewGuid();
+            var price = Money.Create(100, "DKK");
+            var studentId = StudentKey.Create(Guid.NewGuid());
+            var instructorSignature = Signature.Create([0]);
+            var studentSignature = Signature.Create([0]);
+            
+            return TheoryLesson.Create(
+                TheoryLessonKey.Create(id.Value),
+                DrivingSchoolKey.Create(schoolId.Value),
+                dateTime,
+                price,
+                InstructorKey.Create(instructorId.Value),
+                studentId,
+                instructorSignature,
+                studentSignature);
+        }
+    }
+}
