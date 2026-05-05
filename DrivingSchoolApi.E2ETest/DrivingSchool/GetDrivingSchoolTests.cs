7ecba1603243a65f60c8b57a749ecf5b1b2f0fb8
Michael <m_j_39@hotmail.com>
2026-05-05 22:34:49 +0200

    Added test for getting a single driving school and data correctness

--- DrivingSchoolApi.E2ETest/DrivingSchool/GetDrivingSchoolTests.cs
+++ DrivingSchoolApi.E2ETest/DrivingSchool/GetDrivingSchoolTests.cs
@@ -1,19 +1,86 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 using DrivingSchoolApi.DTOs.DrivingSchool;
+using Newtonsoft.Json;
 
 namespace DrivingSchoolApi.E2ETest.DrivingSchool;
 
 public class GetDrivingSchoolTests : TestClass
 {
     [Test]
-    public async Task GetDrivingSchool_Succeeds()
+    public async Task Get_DrivingSchool_Succeeds()
     {
-        
+        // Arrange
+        await AuthService.LoginAsDefaultAdmin();
+
+        var createResponse = await DrivingSchoolService.CreateDrivingSchool(
+            new DrivingSchoolRegistryDto(
+                "Test name",
+                "Test address",
+                "12345678",
+                "Test.com"));
+
+        var createdSchool = JsonConvert.DeserializeObject<DrivingSchoolDto>(
+            await createResponse.Content.ReadAsStringAsync());
+
+        // Act
+        var response = await DrivingSchoolService.GetDrivingSchool(createdSchool.Id);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
     }
     
     [Test]
-    public async Task GetDrivingSchool_Fails_When_ID_DoesntExist()
+    public async Task Get_DrivingSchool_ReturnsCorrectData()
+    {
+        // Arrange
+        await AuthService.LoginAsDefaultAdmin();
+        
+        var createResponse = await DrivingSchoolService.CreateDrivingSchool(
+            new DrivingSchoolRegistryDto(
+                "Test name",
+                "Test address",
+                "12345678",
+                "Test.com"));
+
+        var createdSchool = JsonConvert.DeserializeObject<DrivingSchoolDto>(
+            await createResponse.Content.ReadAsStringAsync());
+
+        // Act
+        var response = await DrivingSchoolService.GetDrivingSchool(createdSchool.Id);
+
+        // Assert
+        var content = await response.Content.ReadAsStringAsync();
+        var school = JsonConvert.DeserializeObject<DrivingSchoolDto>(content);
+
+        Assert.That(school, Is.Not.Null);
+        Assert.That(school!.Id, Is.EqualTo(createdSchool.Id));
+        Assert.That(school.Name, Is.EqualTo("Test name"));
+        Assert.That(school.PhoneNumber, Is.EqualTo("12345678"));
+        Assert.That(school.WebAddress, Is.EqualTo("Test.com"));
+
+        // Nested object
+        Assert.That(school.StreetAddress, Is.Not.Null);
+
+        // Collections
+        Assert.That(school.Packages, Is.Not.Null);
+        Assert.That(school.Packages, Is.Empty);
+        Assert.That(school.Students == null || school.Students.Count >= 0);
+        Assert.That(school.Instructors == null || school.Instructors.Count >= 0);
+    }
+    
+    [Test]
+    public async Task Get_DrivingSchool_ReturnsNotFound_WhenIdDoesNotExist()
+    {
+        // Act
+        var response = await DrivingSchoolService.GetDrivingSchool(Guid.NewGuid());
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+    }
+
+    [Test]
+    public async Task Get_All_DrivingSchools()
     {
         
     }
