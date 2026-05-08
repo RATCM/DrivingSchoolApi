04c3628bb3ed1e78fa71d03d56b66d2545deccd9
Michael <m_j_39@hotmail.com>
2026-05-08 02:55:53 +0200

    fixed latitude to be 90 to -90 was 180 to -180

--- DrivingSchoolApi.Domain/ValueObjects/CoordinatePoint.cs
+++ DrivingSchoolApi.Domain/ValueObjects/CoordinatePoint.cs
@@ -15,7 +15,7 @@ public record CoordinatePoint : ValueObject
     {
         // Validation
         if (order < 1) throw new InvalidInputException("Order cannot be less than 1");
-        if (latitude is > 180 or < -180) throw new InvalidInputException("Latitude must be in the range [-180;180]");
+        if (latitude is > 90 or < -90) throw new InvalidInputException("Latitude must be in the range [-180;180]");
         if (longitude is > 180 or < -180) throw new InvalidInputException("Longitude must be in the range [-180;180]");
 
         return new CoordinatePoint()
